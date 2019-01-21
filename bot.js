25bdfe0541fa63678acd1bf052746331111ca6e5
tom-mindlab <tom@themindlab.co.uk>
2019-01-21 00:16:58 +0000

    some updates

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
-const os = require("os");
 const fs = require("fs");
-const readline = require("readline");
 const Discord = require("discord.js");
+const mkdirp = require("mkdirp");
 
 const bot = new Discord.Client();
 
@@ -13,12 +12,17 @@ const randRange = (min, max) => {
 
 function makeDirForUser(user) {
     return new Promise((res, rej) => {
-        fs.mkdir(`./users/${user.username}`, (err) => {
+        mkdirp(`./users/${user.username}`, (err) => {
             if (err) {
                 rej(new Error(err));
             } else {
                 console.log(`Directory for user ${user.username} created`);
-                fs.writeFile(`./users/${user.username}/creation_dump.json`, JSON.stringify(user), (err) => {
+                fs.writeFile(`./users/${user.username}/creation_dump.json`, JSON.stringify({
+                    date: new Date().toUTCString(),
+                    username: user.username,
+                    id: user.id,
+                    bot: user.bot
+                }), (err) => {
                     if (err) {
                         rej(new Error(err));
                     } else {
@@ -99,7 +103,7 @@ const forceWriteFile = async (path, data) => {
         if (dir_exists) {
             res();
         }
-        fs.mkdir(dir_path, { recursive: true }, (err) => {
+        mkdirp(dir_path, (err) => {
             if (err) {
                 rej(new Error(err));
             }
@@ -117,101 +121,160 @@ const forceWriteFile = async (path, data) => {
     })
 };
 
-
-
-const logMessage = async (username, message) => {
-    console.log(`[${message.channel.name}] ${username}: ${message.content}`);
-
-    const dir_path = `./users/${username}`;
-    const file_path = `${dir_path}/message_log.json`;
-
-    const file_exists = await pathExists(file_path);
-    if (file_exists) {
-        const json_log = await new Promise((res, rej) => {
-            fs.readFile(file_path, (err, data) => {
+const readFileIfExists = async (path) => {
+    return new Promise(async (res, rej) => {
+        if (await pathExists(path)) {
+            fs.readFile(path, (err, data) => {
                 if (err) {
                     rej(new Error(err));
                 }
-                res(JSON.parse(data));
+                res(data);
             })
-        });
-        json_log.messages.push({
-            channel: message.channel.name,
-            content: message.content,
+        } else {
+            res(undefined); // is this odd?
+        }
+    });
+}
+
+const parseMessageContent = async (content) => {
+    
+}
+
+const logMessage = async (message) => {
+    const channame = message.channel.name;
+    const username = message.member.user.username;
+    console.log(`[${message.channel.name}] ${username}: ${message.content}`);
+
+    const user_file_path = `./users/${username}/message_log.json`;
+    const user_data = await readFileIfExists(user_file_path);
+    if (user_data) {
+        console.log(JSON.parse(user_data));
+        const out = JSON.parse(user_data);
+        out.messages.push({
+            date: new Date().toUTCString(),
+            channel: channame,
+            raw_content: message.content,
             mentions: {
-                users: message.mentions.users,
-                members: message.mentions.members,
-                channels: message.mentions.channels,
-                roles: message.mentions.roles
+                users: message.mentions.users.map(v => v.username),
+                channels: message.mentions.channels.map(v => v.name),
+                roles: message.mentions.roles.map(v => v.name)
             }
         });
-        await forceWriteFile(file_path, JSON.stringify(json_log));
+        await forceWriteFile(user_file_path, JSON.stringify(out));
     } else {
-        await forceWriteFile(file_path, JSON.stringify({
+        await makeDirForUser(message.member.user);
+        await forceWriteFile(user_file_path, JSON.stringify({
             created: new Date().toUTCString(),
             username: username,
             messages: [
                 {
-                    channel: message.channel.name,
+                    date: new Date().toUTCString(),
+                    channel: channame,
                     content: message.content,
-                    mentions: {}
+                    mentions: {
+                        users: message.mentions.users.map(v => v.username),
+                        channels: message.mentions.channels.map(v => v.name),
+                        roles: message.mentions.roles.map(v => v.name)
+                    }
                 }
             ]
         }));
     }
+
+    // data redundancy because i just dont care
+    const channel_file_path = `./channels/${channame}/message_log.json`;
+    const channel_data = await readFileIfExists(channel_file_path);
+    if (channel_data) {
+        const out = JSON.parse(channel_data);
+        out.messages.push({
+            date: new Date().toUTCString(),
+            username: username,
+            content: message.content,
+            mentions: {
+                users: message.mentions.users.map(v => v.username),
+                channels: message.mentions.channels.map(v => v.name),
+                roles: message.mentions.roles.map(v => v.name)
+            }
+        });
+        await forceWriteFile(channel_file_path, JSON.stringify(out));
+    } else {
+        await forceWriteFile(channel_file_path, JSON.stringify({
+            created: new Date().toUTCString(),
+            channame: channame,
+            messages: [
+                {
+                    date: new Date().toUTCString(),
+                    username: username,
+                    content: message.content,
+                    mentions: {
+                        users: message.mentions.users.map(v => v.username),
+                        channels: message.mentions.channels.map(v => v.name),
+                        roles: message.mentions.roles.map(v => v.name)
+                    }
+                }
+            ]
+        }))
+    }
+}
+
+const executeCommand = async (command, params) => {
+    switch (command) {
+        case `random`:
+            if (params.length < 2) {
+                message.channel.send(`Need a numeric range \`<min>\` \`<max>\` to produce a random number`);
+            } else {
+                message.channel.send(`${randRange(params[0], params[1])}`)
+            }
+            break;
+        case `status`:
+            if (params.length < 1) {
+                message.channel.send(`Who's status? Missing \`<username>\``);
+            } else {
+                message.channel.send(await (async () => {
+                    try {
+                        const json = JSON.parse(await new Promise((res, rej) => {
+                            fs.readFile(`./users/${params[0]}/status.json`, (err, data) => {
+                                if (err) {
+                                    rej(new Error(err));
+                                } else {
+                                    res(data);
+                                }
+                            });
+                        }));
+                        const last_entry = json.status_updates[json.status_updates.length - 1];
+                        if (last_entry.new_status === `online`) {
+                            return `${params[0]} is online right now! Status last became \`${last_entry.new_status}\` at ${last_entry.date}`;
+                        } else {
+                            return `${params[0]} is offline at the moment. Status became \`${last_entry.new_status}\` at: ${last_entry.date}`;
+                        }
+                    } catch(err) {
+                        return `Something went wrong (I probably have no records for ${params[0]} yet)!`;
+                    }
+                    
+                })());
+            }
+            break;
+        case `choose`:
+            if (params.length < 1) {
+                message.channel.send(`I can't choose if I have no options :robot:`);
+            } else if (params.length === 1) {
+                message.channel.send(`Is this really a choice if I only have one option..?`);
+            } else {
+                message.channel.send(`${params[randRange(0, params.length - 1)]}`)
+            }
+    }
 }
 
 bot.on('message', async (message) => {
-    logMessage(message.member.user.username, message);
+    // console.log(message.mentions);
+    // console.log(message.mentions.users.map(v => v.username));
+    // console.log(message.mentions.channels.map(v => v.name));
+    // console.log(message.mentions.roles.map(v => v.name));
+    logMessage(message);
     if (message.content[0] === `>`) {
         const command = `${message.content.substr(1, message.content.indexOf(` `) - 1)}`;
         const params = message.content.substr(message.content.indexOf(` `) + 1).split(`, `);
-        switch (command) {
-            case `random`:
-                if (params.length < 2) {
-                    message.channel.send(`Need a numeric range \`<min>\` \`<max>\` to produce a random number`);
-                } else {
-                    message.channel.send(`${randRange(params[0], params[1])}`)
-                }
-                break;
-            case `status`:
-                if (params.length < 1) {
-                    message.channel.send(`Who's status? Missing \`<username>\``);
-                } else {
-                    message.channel.send(await (async () => {
-                        try {
-                            const json = JSON.parse(await new Promise((res, rej) => {
-                                fs.readFile(`./users/${params[0]}/status.json`, (err, data) => {
-                                    if (err) {
-                                        rej(new Error(err));
-                                    } else {
-                                        res(data);
-                                    }
-                                });
-                            }));
-                            const last_entry = json.status_updates[json.status_updates.length - 1];
-                            if (last_entry.new_status === `online`) {
-                                return `${params[0]} is online right now! Status last became \`${last_entry.new_status}\` at ${last_entry.date}`;
-                            } else {
-                                return `${params[0]} is offline at the moment. Status became \`${last_entry.new_status}\` at: ${last_entry.date}`;
-                            }
-                        } catch(err) {
-                            return `Something went wrong (I probably have no records for ${params[0]} yet)!`;
-                        }
-                        
-                    })());
-                }
-                break;
-            case `choose`:
-                console.log(`choosing`);
-                if (params.length < 1) {
-                    message.channel.send(`I can't choose if I have no options :robot:`);
-                } else if (params.length === 1) {
-                    message.channel.send(`Is this really a choice if I only have one option..?`);
-                } else {
-                    message.channel.send(`${params[randRange(0, params.length - 1)]}`)
-                }
-        }
+        executeCommand(command, params);
     }
 });
 
